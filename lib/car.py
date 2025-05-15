4d733c214f90ba677590e91eb8db9d0787fef9ad
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-05-15 15:31:41 +0300

    Implement Car and Vehicle classes with initialization and methods for movement and refueling

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,13 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        super().__init__(wheel_size, wheel_number)
+    def go(self):
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        return "filling up!"
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+        pass
